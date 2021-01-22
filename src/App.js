5640a6df507fe0541d8edb445d4f1a05d7bc6dc1
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-22 12:31:09 -0500

    received the data successfully with axios

--- src/App.js
+++ src/App.js
@@ -7,14 +7,31 @@ import styled from 'styled-components';
 //<----- Import axios ----->
 import axios from 'axios';
 //<----- Import character.js ----->
+import Character from './components/Character'
+//<----- Import base url ----->
+import {BASE_URL} from './Constants/secret'
 
------
+
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  //setting the states-fecthing data using from api using axios
+  const [character, setCharacter] = useState([]);
+
+  useEffect (() =>{
+    axios
+    .get(`${BASE_URL}/character`)
+    .then((response =>{
+      console.log(response);
+      setCharacter(response.data.results);
+    }))
+    .catch((error) =>{
+      console.log('Opps! something went wrong! Please try aging!', error);
+    })
+  }, [])
 
   return (
     <div className="App">
