0a0db06632e7739124bdf70f26a9eff8c0236f9e
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-22 12:01:08 -0500

    import part is done for App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+//<----- Import React {useState, useEffect} ----->
+import React, { useState, useEffect } from 'react';
+//<----- Import style sheet App.css ----->
 import './App.css';
+//<----- import styled components ----->
+import styled from 'styled-components';
+//<----- Import axios ----->
+import axios from 'axios';
+//<----- Import character.js ----->
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
